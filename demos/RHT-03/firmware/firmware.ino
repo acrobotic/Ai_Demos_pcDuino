42b45c23a057d0fb9aecdb51e6fea36836fe3ec8
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2014-10-07 07:27:13 -0700

    add hook-up instructions and improve code readability and output

--- demos/RHT-03/firmware/firmware.ino
+++ demos/RHT-03/firmware/firmware.ino
@@ -1,16 +1,28 @@
+/* Digital Humidity and Temperature Sensor pcDuino Test Program
+ *
+ * Note: the RHT-03 is also known as the DHT-22 sensor.
+ *
+ *  1. Connect from left to right:
+ *   1. Pin 1 to 3.3 V
+ *   2. Pin 2 to ADC5 (J12.6)
+ *   3. Pin 3 Not Connected
+ *   4. Pin 4 to GND (J9.6)
+ *  2. Connect a pull-up 10k Ohm resistor from signal pin 2 to Vdd pin 1.
+ *
+ */
 #include "dht.h"
 
 dht DHT;
 
 #define DHT22_PIN 5
+#define DHT22_SAMPLE_RATE_MAX_MS 2*1000
 
 void setup()
 {
-    Serial.begin(115200);
-    printf("DHT TEST PROGRAM ");
-    //printf("LIBRARY VERSION: ");
-    //printf(DHT_LIB_VERSION);
-    //printfln("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
+    printf("DHT TEST PROGRAM\n");
+    printf("LIBRARY VERSION: ");
+    printf(DHT_LIB_VERSION);
+    printf("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)\n");
 }
 
 void loop()
@@ -37,15 +49,12 @@ void loop()
         printf("Unknown error,\t");
         break;
     }
-    // DISPLAY DATA
+
     printf("%f", DHT.humidity);
     printf(",\t");
     printf("%f", DHT.temperature);
     printf(",\t");
     printf("%f\n", stop - start);
 
-    delay(2000);
+    delay(DHT22_SAMPLE_RATE_MAX_MS);
 }
-//
-// END OF FILE
-//
