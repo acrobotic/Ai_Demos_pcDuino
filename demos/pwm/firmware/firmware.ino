f16d74ea081e4bf2f19642954deba3c480a72213
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2014-10-09 22:47:06 -0700

    PWM test for oscilloscopes.

--- demos/pwm/firmware/firmware.ino
+++ demos/pwm/firmware/firmware.ino
@@ -1,36 +1,38 @@
 /*
-* PWM test program
-*/
+ * pcDuino PWM Signal Test Program
+ *
+ * 1. Connect PWM5/GPIO5 (J11.6) to an oscilloscope. Observe duty cycle 
+ *    change from max to min.
+ * 2. Alternatively, connect an LED through a resistor. Observe the 
+ *    LED go from bright to dim.
+ *
+ */
 #include <core.h>
+
 int pwm_id = 5;
-int delay_us = 200000;
-int freq = 781; //Hz
-int value =  MAX_PWM_LEVEL/2;
+int delay_us = 200*1000;
+int freq = 781;
+int value = MAX_PWM_LEVEL;
+
 void setup()
 {
-    int step = 0;
-#ifndef PCDUINO_IDE
-    if ( argc != 4 )
-    {
-        printf("Usage %s PIN_ID(3/9/10/11) Frequency[125-2000]Hz Duty Level or PIN_ID(5/6) Frequency[195,260,390,520,781]Hz Duty Level\n ", argv[0]);
-        exit(-1);   
-    }
-    
-    pwm_id = atoi(argv[1]);
-    freq = atoi(argv[2]);
-    value = atoi(argv[3]); 
-#endif
+}
 
+void set_pwm()
+{
+    int step = 0;
     step = pwmfreq_set(pwm_id, freq);
-    printf("PWM%d set freq %d and valid duty cycle range [0, %d]\n", pwm_id, freq, step);
-    if (step > 0){
-       printf("PWM%d test with duty cycle %d\n", pwm_id, value);
-       analogWrite(pwm_id, value);
-    }
+    printf("Set PWM%d freq %d Hz, duty cycle %d (range 0..%d)\n", 
+            pwm_id, freq, value, step);
+    analogWrite(pwm_id, value);
 }
 
 void loop()
 {
+    set_pwm();
     delayMicroseconds(delay_us); 
+    value -= 10;
+    if (value < 1)
+        value = MAX_PWM_LEVEL;
 }
 
